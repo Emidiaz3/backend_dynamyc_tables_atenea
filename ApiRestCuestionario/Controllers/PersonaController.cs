e973cf6fa9cd7133b7a03370eed0d2ff5a16498f
Leoo2608 <leonardo.antazu@upeu.edu.pe>
2024-01-23 17:12:01 -0500

    ins per

--- ApiRestCuestionario/Controllers/PersonaController.cs
+++ ApiRestCuestionario/Controllers/PersonaController.cs
@@ -15,6 +15,17 @@ using System.Threading.Tasks;
 
 namespace ApiRestCuestionario.Controllers
 {
+    public class MultipleInsertPersona
+    {
+        public Persona person { get; set; }
+        public List<InsertPersonaOrganization> insertPersonaOrganizacionList { get; set; }
+    }
+
+    public class InsertPersonaOrganization
+    {
+        public int IdOrganizacion { get; set; }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class PersonaController : ControllerBase
@@ -104,6 +115,76 @@ namespace ApiRestCuestionario.Controllers
             }
 
         }
+
+        [HttpPost("MultipleInsertPerson")]
+        public async Task<ActionResult> MultipleInsertPerson([FromBody] JsonElement value)
+        {
+            try
+            {
+                List<MultipleInsertPersona> list = JsonConvert.DeserializeObject<List<MultipleInsertPersona>>(value.ToString());
+
+                var gruposPorProyecto = list.GroupBy(x => x.person.Proyecto)
+                                             .Select(group => new
+                                             {
+                                                 Proyecto = group.Key,
+                                                 Codigos = String.Join(",", group.Select(g => g.person.Id_Persona))
+                                             });
+
+
+
+                foreach (var grupo in gruposPorProyecto)
+                {
+                    await context.Database.ExecuteSqlInterpolatedAsync($@"EXEC [dbo].[SP_CHECK_PERSONA]
+                    @Codigos={grupo.Codigos},
+                    @Proyecto={grupo.Proyecto};");
+                }
+
+
+                foreach (MultipleInsertPersona insertPersona in list)
+                {
+                    var idParameter = new SqlParameter("@Id", SqlDbType.Int)
+                    {
+                        Direction = ParameterDirection.InputOutput,
+                        Value = insertPersona.person.Id ?? (object)DBNull.Value
+                    };
+
+                    await context.Database.ExecuteSqlInterpolatedAsync($@"EXEC [dbo].[SP_GUARDAR_PERSONA] 
+                        @Id={idParameter} OUTPUT, 
+                        @Id_Persona={insertPersona.person.Id_Persona},
+                        @NomPersona={insertPersona.person.NomPersona},
+                        @Proyecto={insertPersona.person.Proyecto},
+                        @Localidad={insertPersona.person.Localidad},
+                        @Nivel_Riesgo_General={insertPersona.person.Nivel_Riesgo_General},
+                        @Latitud={insertPersona.person.Latitud},
+                        @Longitud={insertPersona.person.Longitud},
+                        @TipoDocumento={insertPersona.person.TipoDocumento},
+                        @NumeroDocumento={insertPersona.person.NumeroDocumento},
+                        @Estado={insertPersona.person.Estado},
+                        @IdUsuario={insertPersona.person.IdUsuario};");
+
+
+                    foreach (InsertPersonaOrganization insert in insertPersona.insertPersonaOrganizacionList)
+                    {
+                        await context.Database.ExecuteSqlInterpolatedAsync($@"EXEC [dbo].[SP_GUARDAR_PERSONA_ORGANIZACION] 
+                            @IdPersona={idParameter},
+                            @IdOrganizacion={insert.IdOrganizacion},
+                            @Proyecto={insertPersona.person.Proyecto};");
+                    }
+                }
+
+                return StatusCode(200, new ItemResp { status = 200, message = CONFIRM, data = null });
+            }
+            catch (InvalidCastException e)
+            {
+                return StatusCode(404, new ItemResp { status = 200, message = e.ToString(), data = e.ToString() });
+            }
+            catch (SqlException ex)
+            {
+                return StatusCode(500, new { status = 500, message = ex.Message });
+            }
+
+        }
+
         //[Route("EditPerson")]
         //[HttpPost]
         //public ActionResult EditPerson([FromBody] JsonElement value)
@@ -122,7 +203,7 @@ namespace ApiRestCuestionario.Controllers
         //    }
 
         //}
-        
+
         [HttpGet]
         [Route("GetPerson")]
         public async Task<ActionResult> GetPerson(int IdProyecto)
